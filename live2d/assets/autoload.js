94a5b2c04e7acf2381c8c8ac3aeb3fa76905229d
zhidehuiweii <zhidehuiweii@163.com>
2019-03-05 16:17:55 +0800

    a

--- live2d/assets/autoload.js
+++ live2d/assets/autoload.js
@@ -5,9 +5,11 @@ try {
     $.ajax({url: 'assets/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
     /* 可直接修改部分参数 */
     live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
-    live2d_settings['modelId'] = 5;                  // 默认模型 ID
+    live2d_settings['modelId'] = 6;                  // 默认模型 ID
     live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
     live2d_settings['modelStorage'] = false;         // 不储存模型 ID
+    live2d_settings['waifuEdgeSide'] = 'right:30';
+    live2d_settings['aboutPageUrl']         = '/';
     /* 在 initModel 前添加 */
     initModel('assets/waifu-tips.json');
 } catch(err) { console.log('[Error] JQuery is not defined.') }
